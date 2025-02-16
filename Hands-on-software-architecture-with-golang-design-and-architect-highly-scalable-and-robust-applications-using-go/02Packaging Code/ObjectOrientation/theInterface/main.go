2578a6067b3262498b087def823f992c0781e051
artitaya <artitaya2466@gmail.com>
2025-02-16 21:19:43 +0700

    feat : the interface

--- Hands-on-software-architecture-with-golang-design-and-architect-highly-scalable-and-robust-applications-using-go/02Packaging Code/ObjectOrientation/theInterface/main.go
+++ Hands-on-software-architecture-with-golang-design-and-architect-highly-scalable-and-robust-applications-using-go/02Packaging Code/ObjectOrientation/theInterface/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type LatLong struct {
 	Lat  float64
 	Long float64
@@ -9,6 +11,92 @@ type Animal interface {
 	GetLocaltion() LatLong
 	SetLocation(LatLong)
 	CanFly() bool
-	Spaek()
+	GetName() string
+	Speak() string
 }
 
+type Animals struct {
+	Lion   Lion
+	Pigeon Pigeon
+}
+
+// The Lion Family
+type Lion struct {
+	name       string
+	maneLength int
+	location   LatLong
+}
+
+func (lion *Lion) GetLocaltion() LatLong {
+	return lion.location
+}
+
+func (lion *Lion) SetLocation(loc LatLong) {
+	lion.location = loc
+}
+
+func (lion *Lion) CanFly() bool {
+	return false
+}
+
+func (lion *Lion) Spaek() string {
+	return "roar"
+}
+
+func (lion *Lion) GetManeLength() int {
+	return lion.maneLength
+}
+
+func (lion *Lion) GetName() string {
+	return lion.name
+}
+
+// The Pigeon Family
+type Pigeon struct {
+	name     string
+	location LatLong
+}
+
+func (p *Pigeon) GetLocaltion() LatLong {
+	return p.location
+}
+
+func (p *Pigeon) SetLocation(loc LatLong) {
+	p.location = loc
+}
+
+func (p *Pigeon) CanFly() bool {
+	return false
+}
+
+func (p *Pigeon) Spaek() string {
+	return "hoot"
+}
+
+func (p *Pigeon) GetName() string {
+	return p.name
+}
+
+// The symphony
+func makeThemSing(animals []Animal) {
+	for _, animal := range animals {
+		fmt.Println(animal.GetName() + " says " + animal.Speak())
+	}
+}
+
+func main() {
+	var aAnimal Animals
+	aAnimal = Animals{
+		Lion: Lion{
+			name:       "Leo",
+			maneLength: 10,
+			location:   LatLong{10.40, 11.5},
+		},
+		Pigeon: Pigeon{
+			name:     "Pigeon",
+			location: LatLong{10.40, 11.5},
+		},
+	}
+	fmt.Println(aAnimal)
+
+}
