c2ff06556289de21799958a29c3270867bc9f739
artitaya <artitaya2466@gmail.com>
2025-03-16 21:09:54 +0700

    feat : embedding

---
+++ Hands-on-software-architecture-with-golang-design-and-architect-highly-scalable-and-robust-applications-using-go/02Packaging Code/ObjectOrientation/embedding/embedding.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+type Bird struct {
+	featherLength  int
+	classification string
+}
+
+type Pigeon struct {
+	Bird
+	Name string
+}
+
+func main() {
+	p := Pigeon{
+		Name: "Tweety",
+	}
+	p.featherLength = 10
+	fmt.Println(p)
+
+}
