a37df8e58023cd8e97e34b0b3fb18e660111f755
artitaya <artitaya2466@gmail.com>
2025-03-25 23:06:20 +0700

    feat : arrays

--- DataStructure/02gettingStarted/arrays/arrays.go
+++ DataStructure/02gettingStarted/arrays/arrays.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var arr = [5]int{1, 2, 4, 5, 6}
+	for i := 0; i < len(arr); i++ {
+		fmt.Println("printing elements ", arr[i])
+	}
+
+	for i := range arr {
+		fmt.Println(" range ", i)
+	}
+
+	for _, value := range arr {
+		fmt.Println("blank range ", value)
+	}
 }
