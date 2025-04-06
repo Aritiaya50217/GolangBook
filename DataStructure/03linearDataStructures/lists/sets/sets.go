41d737f927daf2810135c1b601aabf3d36b29a0e
artitaya <artitaya2466@gmail.com>
2025-04-06 21:09:52 +0700

    feat : intersectSet and unionSet

--- DataStructure/03linearDataStructures/lists/sets/sets.go
+++ DataStructure/03linearDataStructures/lists/sets/sets.go
@@ -30,17 +30,56 @@ func (set *Set) ContainsElement(element int) bool {
 	return exists
 }
 
+// Intersect method returns the set which intersects with anotherSet
+func (set *Set) Intersect(anotherSet *Set) *Set {
+	intersectSet := &Set{}
+	intersectSet.New()
+	for value, _ := range set.integerMap {
+		if anotherSet.ContainsElement(value) {
+			intersectSet.AddElement(value)
+		}
+	}
+	return intersectSet
+}
+
+// Union method returns the set which is union of the set with anotherSet
+func (set *Set) Union(anotherSet *Set) *Set {
+	var unionSet = &Set{}
+	unionSet.New()
+	for value, _ := range set.integerMap {
+		unionSet.AddElement(value)
+	}
+
+	for value, _ := range anotherSet.integerMap {
+		unionSet.AddElement(value)
+	}
+
+	return unionSet
+}
+
 func main() {
-	set := Set{}
+	set := &Set{}
 	set.New()
 	set.AddElement(1)
 	set.AddElement(2)
 	fmt.Println("sets : ", set)
+
 	// delete
-	set.DeleteElement(2)
-	fmt.Println("After deleted : ", set)
+	// set.DeleteElement(2)
+	// fmt.Println("After deleted : ", set)
 
 	// check  element
-	fmt.Println("2 in set : ", set.ContainsElement(2))
+	fmt.Println("initial set", set)
+	fmt.Println("1 in set : ", set.ContainsElement(1))
 
+	// intersect
+	anotherSet := &Set{}
+	anotherSet.New()
+	anotherSet.AddElement(2)
+	anotherSet.AddElement(4)
+	anotherSet.AddElement(5)
+	fmt.Println("interSect : ", set.Intersect(anotherSet))
+
+	// union
+	fmt.Println(set.Union(anotherSet))
 }
