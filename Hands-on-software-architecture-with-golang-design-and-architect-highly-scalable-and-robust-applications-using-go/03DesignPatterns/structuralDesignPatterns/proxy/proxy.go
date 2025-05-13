6bd97fdd0d63bee9e2111c634ad78329f1768d2e
artitaya <artitaya2466@gmail.com>
2025-05-13 16:35:04 +0700

    feat : proxy

---
+++ Hands-on-software-architecture-with-golang-design-and-architect-highly-scalable-and-robust-applications-using-go/03DesignPatterns/structuralDesignPatterns/proxy/proxy.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+// Proxy
+type HotelBoutiqueProxy struct {
+	subject *HotelBoutique
+}
+
+func (p *HotelBoutiqueProxy) Book() {
+	if p.subject == nil {
+		p.subject = new(HotelBoutique)
+	}
+	fmt.Println("Proxy Delegation Booking call")
+
+	// The API call will happen here
+	// For example sake a simple delegation is implemented
+	p.subject.Book()
+}
+
+// Dummy Subject
+type HotelBoutique struct{}
+
+func (s *HotelBoutique) Book() {
+	fmt.Println("Booking done on external site")
+}
+
+func main() {
+	var hotel HotelBoutiqueProxy
+	hotel.Book()
+}
