0395f0c6a1da715092422d6b77fa362a16f96a5b
artitaya <artitaya2466@gmail.com>
2025-07-05 21:26:56 +0700

    cancel

---
+++ EffectiveConcurrency/chapter09AtomicMemoryOperations/practicalUsesOfAtomics/cancellations/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+func CancelSupport() (cancel func(), isCancelled func() bool) {
+	v := atomic.Bool{}
+	cancel = func() {
+		v.Store(true)
+	}
+	isCancelled = func() bool {
+		return v.Load()
+	}
+	return
+
+}
+
+func main() {
+	cancel, isCancelled := CancelSupport()
+	wg := sync.WaitGroup{}
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for {
+			time.Sleep(100 * time.Millisecond)
+			if isCancelled() {
+				fmt.Println("Cancelled")
+				return
+			}
+		}
+	}()
+	time.AfterFunc(5*time.Second, cancel)
+	wg.Wait()
+}
