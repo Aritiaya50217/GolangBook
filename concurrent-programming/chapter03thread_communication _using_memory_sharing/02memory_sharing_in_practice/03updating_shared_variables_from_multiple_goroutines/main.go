c8539c1264bd3bcf9b26d291bd77e2c4f0d233d0
artitaya <artitaya2466@gmail.com>
2025-10-28 21:26:36 +0700

    enhance : updating shared variables from multiple goroutines

--- concurrent-programming/chapter03thread_communication _using_memory_sharing/02memory_sharing_in_practice/03updating_shared_variables_from_multiple_goroutines/main.go
+++ concurrent-programming/chapter03thread_communication _using_memory_sharing/02memory_sharing_in_practice/03updating_shared_variables_from_multiple_goroutines/main.go
@@ -1,23 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"strings"
+)
+
+const allLetters = "abcdefghijklmnopqrstuvwxyz"
+
+func countLetters(url string, frequency []int) {
+	resp, _ := http.Get(url) // downloads the web page from the given URL
+	defer resp.Body.Close()  // close the response at the end of the function
+
+	if resp.StatusCode != 200 {
+		panic("Server returning error status code : " + resp.Status)
+	}
+	body, _ := io.ReadAll(resp.Body)
+	for _, b := range body { // iterates over every downloaded character
+		c := strings.ToLower(string(b))
+		cIndex := strings.Index(allLetters, c) // finds the index of the character in the alphabet
+		if cIndex >= 0 {
+			frequency[cIndex] += 1 // if the character is part of the alphabet , increments the count by 1
+		}
+	}
+	fmt.Println("Completed : ", url)
+}
 
 func main() {
-	countChan := make(chan int)
-	done := make(chan bool)
-
-	go func() {
-		count := 0
-		for delta := range countChan {
-			count += delta
-		}
-		fmt.Println("Final Count : ", count)
-		done <- true
-	}()
-
-	for i := 0; i < 5; i++ {
-		countChan <- 1
+	var frequency = make([]int, 26) // initialzes slice space for the frequency table
+	for i := 1000; i <= 1030; i++ { // iterates from document ID 1000 to 1030 to download 31 docs
+		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%d.txt", i)
+		countLetters(url, frequency) // calls the countLetters() function sequentially
 	}
-	close(countChan)
-	<-done
+
+	for i, c := range allLetters {
+		fmt.Printf("%c-%d ", c, frequency[i]) // Outputs each letter with its frequency
+	}
 }
