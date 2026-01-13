32542783adf12fa91a74efecff8478a260d5d5b5
artitaya <artitaya2466@gmail.com>
2026-01-13 16:00:55 +0700

    no message

--- concurrent-programming/chapter08selecting_channels/01combining_multiple_channels/06disabling_select_cases_with_nil_channels/main.go
+++ concurrent-programming/chapter08selecting_channels/01combining_multiple_channels/06disabling_select_cases_with_nil_channels/main.go
@@ -1,3 +1,23 @@
 package main
 
-196
+import (
+	"math/rand"
+	"time"
+)
+
+func generateAmounts(n int) <-chan int {
+	amounts := make(chan int) // creates an output channel
+	go func() {
+		defer close(amounts) // closes
+		for i := 0; i < n; i++ {
+			amounts <- rand.Intn(100) + 1
+			time.Sleep(100 * time.Millisecond)
+		}
+	}()
+	return amounts // return
+
+}
+
+func main() {
+	// 199
+}
