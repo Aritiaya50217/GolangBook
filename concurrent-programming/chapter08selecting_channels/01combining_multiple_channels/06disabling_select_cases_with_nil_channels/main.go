46b339e3454d2605274ecdf17ec54b2d62ee635e
artitaya <artitaya2466@gmail.com>
2026-01-28 21:13:24 +0700

    disabling_select_cases_with_nil_channels

--- concurrent-programming/chapter08selecting_channels/01combining_multiple_channels/06disabling_select_cases_with_nil_channels/main.go
+++ concurrent-programming/chapter08selecting_channels/01combining_multiple_channels/06disabling_select_cases_with_nil_channels/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
 	"time"
 )
@@ -14,10 +15,32 @@ func generateAmounts(n int) <-chan int {
 			time.Sleep(100 * time.Millisecond)
 		}
 	}()
-	return amounts // return
+	return amounts // return the output channel
 
 }
 
 func main() {
-	// 199
+	sales := generateAmounts(50)    // generates 50 amounts on the sales channel
+	expenses := generateAmounts(40) // generates 40 amounts on the expenses channel
+	endOfDayAmount := 0
+
+	for sales != nil || expenses != nil { // continues to loop while there is a non-nil channel
+		select {
+		case sale, moreData := <-sales: // consumes the next amount and channel open flag from the sales channel
+			if moreData {
+				fmt.Println("Sale of : ", sale)
+				endOfDayAmount += sale // adds the sales amount to the total end-of-day balance
+			} else {
+				sales = nil // if the channel has been cloesd , marks the channel as nil , disabling this select case
+			}
+		case expense, moreData := <-expenses: // consumes the next amount and channel open floag from the expenses channel
+			if moreData {
+				fmt.Println("Expense of : ", expense)
+				endOfDayAmount -= expense // subtracts the expense amount from the total end-of-day balance
+			} else {
+				expenses = nil // if the channel has been closed, marks the channel as nil , disabling this select case
+			}
+		}
+	}
+	fmt.Println("End of day profit and loss : ", endOfDayAmount)
 }
