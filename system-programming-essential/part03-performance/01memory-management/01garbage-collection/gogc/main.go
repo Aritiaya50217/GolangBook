34db9db92cd6093d4ca43825f13350a3031da82e
artitaya <artitaya2466@gmail.com>
2026-05-30 20:10:34 +0700

    update

--- system-programming-essential/part03-performance/01memory-management/01garbage-collection/gogc/main.go
+++ system-programming-essential/part03-performance/01memory-management/01garbage-collection/gogc/main.go
@@ -12,5 +12,5 @@ func main() {
 	fmt.Println("done")
 
 	// GOGC=20 GODEBUG=gctrace=1 go run main.go  --> GC บ่อย ,RAM น้อย, CPU มาก
-	// GOGC=10000 GODEBUG=gctrace=1 go run main.go -->  GC น้อย, RAM มาก, CPU น้อย
+	// GOGC=100 GODEBUG=gctrace=1 go run main.go -->  GC น้อย, RAM มาก, CPU น้อย
 }
