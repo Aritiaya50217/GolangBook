f51498dd0685f7f2fd270087b7ea648e530661a3
artitaya <artitaya2466@gmail.com>
2026-06-06 21:16:19 +0700

    graceful shutdown

---
+++ system-programming-essential/part02interaction-with-the-os/03working-with-system-events/01managing-system-events/02graceful-shutdown/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	server := &http.Server{
+		Addr: ":8080",
+	}
+	go server.ListenAndServe()
+
+	stop := make(chan os.Signal, 1)
+
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	<-stop
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	server.Shutdown(ctx)
+}
