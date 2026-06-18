7cdf8e151fb5e8c6d0731750c9779613054f7ac3
artitaya <artitaya2466@gmail.com>
2026-06-18 20:17:07 +0700

    liskov substitution principle

--- Hands-on-software-architecture-with-golang-design-and-architect-highly-scalable-and-robust-applications-using-go/chapter03design-patterns/01design-principles/03liskov-substitution-principle/main.go
+++ Hands-on-software-architecture-with-golang-design-and-architect-highly-scalable-and-robust-applications-using-go/chapter03design-patterns/01design-principles/03liskov-substitution-principle/main.go
@@ -1,1 +1,24 @@
 package main
+
+type User struct{}
+
+type UserRepository interface {
+	FindByID(id uint) (*User, error)
+}
+
+type MySQLRepo struct{}
+
+func (r MySQLRepo) FindByID(id uint) (*User, error)
+
+type MongoRepo struct{}
+
+func (r MongoRepo) FindByID(id uint) (*User, error)
+
+type MockRepo struct{}
+
+func (r MockRepo) FindByID(id uint) (*User, error)
+
+// Usecase ควรทำงานเหมือนเดิม ไม่ว่าจะ inject MySQLRepo , MongoRepo , MockRepo
+type UserUsecase struct {
+	repo UserRepository
+}
